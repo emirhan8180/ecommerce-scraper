2842cf6dd37b28ec496490c0c401271fa4038375
8180e <emirceylan31@hotmail.com>
2026-05-29 16:39:19 +0200

    feat: scrape pages in parallel

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use std::fs;
+use std::fs::File;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
+use futures::{StreamExt, TryStreamExt, stream};
 use scraper::{ElementRef, Html, Selector};
 use serde::{Deserialize, Serialize};
 
-struct ProductDataSelectors<'a> {
-    link: &'a Selector,
-    img: &'a Selector,
-    title: &'a Selector,
-    price: &'a Selector,
+struct ProductDataSelectors {
+    link: Selector,
+    img: Selector,
+    title: Selector,
+    price: Selector,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -21,10 +22,10 @@ struct ProductData {
 
 impl ProductData {
     fn get_from_element(product: ElementRef, selectors: &ProductDataSelectors) -> Option<Self> {
-        let a = product.select(selectors.link).next()?;
-        let img = product.select(selectors.img).next()?;
-        let h2 = product.select(selectors.title).next()?;
-        let price = product.select(selectors.price).next()?;
+        let a = product.select(&selectors.link).next()?;
+        let img = product.select(&selectors.img).next()?;
+        let h2 = product.select(&selectors.title).next()?;
+        let price = product.select(&selectors.price).next()?;
 
         let price: String = price.text().collect();
 
@@ -37,33 +38,53 @@ impl ProductData {
     }
 }
 
+async fn get_product_data(
+    page: usize,
+    client: &reqwest::Client,
+    product_selector: &Selector,
+    selectors: &ProductDataSelectors,
+) -> Result<Vec<ProductData>> {
+    let url = format!("https://www.scrapingcourse.com/ecommerce/page/{page}");
+    let response = client
+        .get(&url)
+        .send()
+        .await
+        .with_context(|| format!("request failed for {url}"))?
+        .error_for_status()?
+        .text()
+        .await?;
+
+    let document = Html::parse_document(&response);
+    let products = document.select(product_selector);
+
+    Ok(products
+        .filter_map(|product| ProductData::get_from_element(product, selectors))
+        .collect())
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    let client = reqwest::Client::new();
+
     let product_selector = Selector::parse("#product-list > li").unwrap();
 
     let selectors = ProductDataSelectors {
-        link: &Selector::parse("a").unwrap(),
-        img: &Selector::parse("img").unwrap(),
-        title: &Selector::parse("h2").unwrap(),
-        price: &Selector::parse(".price").unwrap(),
+        link: Selector::parse("a").unwrap(),
+        img: Selector::parse("img").unwrap(),
+        title: Selector::parse("h2").unwrap(),
+        price: Selector::parse(".price").unwrap(),
     };
 
-    let client = reqwest::Client::new();
+    let products = stream::iter(1..=12)
+        .map(|page| get_product_data(page, &client, &product_selector, &selectors))
+        .buffer_unordered(5)
+        .try_fold(vec![], |mut acc, page_products| async {
+            acc.extend(page_products);
+            Ok(acc)
+        })
+        .await?;
 
-    let mut data = vec![];
-    for page in 0..13 {
-        let url = format!("https://www.scrapingcourse.com/ecommerce/page/{page}");
-        let response = client.get(url).send().await?.text().await?;
-
-        let document = Html::parse_document(&response);
-        let products = document.select(&product_selector);
-
-        data.extend(
-            products.filter_map(|product| ProductData::get_from_element(product, &selectors)),
-        );
-    }
-
-    fs::write("./data.json", serde_json::to_string(&data)?)?;
+    serde_json::to_writer(File::create("./data.json")?, &products)?;
 
     Ok(())
 }
