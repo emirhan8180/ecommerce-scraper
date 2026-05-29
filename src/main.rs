0f749239c68bb9b0712cda0d0582d2bd028a8c50
8180e <emirceylan31@hotmail.com>
2026-05-29 08:54:25 +0200

    feat: add getting page content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let response = reqwest::Client::new()
+        .get("https://www.scrapingcourse.com/ecommerce")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("{response}");
+
+    Ok(())
 }
