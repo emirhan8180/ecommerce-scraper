933803de97f8e4a3855a43d696f40a7fa486d7c2
8180e <emirceylan31@hotmail.com>
2026-05-29 09:50:53 +0200

    feat: add getting the complete product data

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use anyhow::Result;
 use scraper::{Html, Selector};
 
+#[derive(Debug)]
+struct ProductData<'a> {
+    url: &'a str,
+    image: &'a str,
+    name: String,
+    price: f32,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let response = reqwest::Client::new()
@@ -11,11 +19,38 @@ async fn main() -> Result<()> {
         .await?;
 
     let document = Html::parse_document(&response);
-    let selector = Selector::parse("#product-list > li :is(img, h2, bdi)").unwrap();
 
-    for element in document.select(&selector) {
-        println!("{element:?}");
-    }
+    let product_selector = Selector::parse("#product-list > li").unwrap();
+
+    let link_selector = Selector::parse("a").unwrap();
+    let img_selector = Selector::parse("img").unwrap();
+    let title_selector = Selector::parse("h2").unwrap();
+    let price_selector = Selector::parse(".price").unwrap();
+
+    let data: Vec<_> = document
+        .select(&product_selector)
+        .filter_map(|product| {
+            let a = product.select(&link_selector).next()?;
+            let img = product.select(&img_selector).next()?;
+            let h2 = product.select(&title_selector).next()?;
+            let bdi = product.select(&price_selector).next()?;
+
+            Some(ProductData {
+                url: a.value().attr("href")?,
+                image: img.value().attr("src")?,
+                name: h2.text().collect(),
+                price: bdi
+                    .text()
+                    .collect::<String>()
+                    .replace('$', "")
+                    .trim()
+                    .parse()
+                    .ok()?,
+            })
+        })
+        .collect();
+
+    println!("{data:?}");
 
     Ok(())
 }
